1974c1e4d9f8f3d6e60b90a6d12d02e638261972
bsantanad <0211518@up.edu.mx>
2022-12-08 15:26:48 -0600

    fixed some grammar
    probably still have to fix even more grammar
    
    Signed-off-by: bsantanad <0211518@up.edu.mx>

--- public/posts/store-sales.php
+++ public/posts/store-sales.php
@@ -29,18 +29,18 @@
 <small><a href='../blog.html'>back</a></small>
 <article>
 <h1>store sales - data visualization</h1>
-<p>hello there, in this post you'll find historical information about the sales by
-the store <em>Favorita</em> down in Ecuador. The information will be organized in
-colorful charts and pretty plots, hopefully, I can show you some insights
+<p>hello there, in this post you'll find historical information about sales made by
+the store <em>Favorita</em> down in Ecuador (or maybe up I don't know where you live). The information will be organized in
+colorful charts and pretty plots, and hopefully, I'll be able to show you some insights
 into the store's behavior throughout the years.</p>
 <p>Let's start by checking the information we will be analyzing.</p>
 <h2>data</h2>
 <p>We have some <code>csv</code>'s files with some relevant information about the store. All
 the files here go from 2013 to mid-2017. We got this information
 from <a href="https://www.kaggle.com/competitions/store-sales-time-series-forecasting/data">kaggle.com</a>, so if you want to know more about what is in the
-files or even download them you can go <a href="https://www.kaggle.com/competitions/store-sales-time-series-forecasting/data">over there</a> and check them for
+files or download them you can go <a href="https://www.kaggle.com/competitions/store-sales-time-series-forecasting/data">over there</a> and check them for
 yourself.</p>
-<p>In case you are too lazy for that (I do not blame you), here is a summary of
+<p>In case you are too lazy for to do that (I do not blame you), here is a summary of
 what information we have:</p>
 <ul>
 <li>the price of oil throughout the period specified above. </li>
@@ -59,21 +59,21 @@ located the top performing stores.</p>
 <p>After that, we will see how the top families/categories of products perform
 throughout the year.</p>
 <p>Finally, let's see if we can find a relationship between holidays and
-transactions.</p>
+sales.</p>
 <p>In case you like bullet points better:<p>
 <ul>
 <li>oil vs transactions </li>
 <li>map ecuador top stores </li>
 <li>top product categories throughout the year </li>
-<li>holidays vs transactions </li>
+<li>holidays vs sales</li>
 </ul>
 <h2>is there a relationship between oil and transactions?</h2>
 <p>well, the post said Ecuador is highly dependent on oil for its economy, let us
-see, how well that holds up when we analyze the information.</p>
+see how well that holds up when we analyze the information.</p>
 <h3>transactions</h3>
-<p>So we have a loooot of information, on the transactions, meaning we have the
-daily transaction for each day. So first let's get the average number of
-transactions per month. Reducing the data a lot and letting us handle it
+<p>So we have a loooot of information on transactions, meaning we have the
+daily transactions made each day. So first let's get the average number of
+transactions per month. Reducing the data and letting us handle it
 better.</p>
 <p>in case you want to see how to do this:</p>
 <pre><code class="language-python">t = pd.read_csv('transactions.csv', parse_dates = ['date'])
@@ -87,7 +87,7 @@ seaborn for the chars/plots)</p>
 
 <p>Although this plot looks nice, we can not make that many conclusions about
 it, we could maybe say that every time the last month of the year comes, the
-sales go up by a lot, but by how much? Let us get the percentage change
+sales go up by a lot, but, by how much? Let us get the percentage change
 month by month, meaning the difference between two months, and see how much
 the transactions went up or down, see if there is a pattern or something</p>
 <div class='plot'>
@@ -96,7 +96,7 @@ the transactions went up or down, see if there is a pattern or something</p>
 
 <p>Okay, now we can see how when December comes, it always comes up by the same
 amount we can confirm what we thought in the last plot, that this data has
-seasonality, when December comes the transactions grow to the
+seasonality, when December comes the transactions grow in respect to the
 last month approximately, by 25 percent.</p>
 <p>Now let us see if the oil has a behavior that looks something similar</p>
 <h3>oil</h3>
@@ -105,8 +105,8 @@ last month approximately, by 25 percent.</p>
 <?php include  __DIR__.'/../static/plots/oil_a.html'; ?>
 </div>
 
-<p>Well at first it does not seem to have that much in common with the first one
-we do not see the same seasonality. But hey, who are we to judge let's do
+<p>Well at first it does not seem to have that much in common with the first one.
+We do not see the same seasonality. But hey, who are we to judge let's do
 the percentage change thing and see if it looks like the transactions one.</p>
 <div class='plot'>
 <?php include  __DIR__.'/../static/plots/oil_b.html'; ?>
@@ -118,7 +118,7 @@ the percentage change thing and see if it looks like the transactions one.</p>
 </div>
 
 <p>yeap, definitely no.</p>
-<p>Let's just do a correlation table, to make sure we are not imagining things.</p>
+<p>Let's just do a correlation table, just to make sure we are not imagining things.</p>
 <pre><code class="language-python"># where d and o are the transactions and oil dataframes
 j = pd.merge(d, o, right_on = 'date', left_on = 'date')
 j.corr()
@@ -157,8 +157,8 @@ top = top.tail(10)
 | 8 | 45        | 5.449801E+07 | Quito     | pichincha  | A    | 11      |
 | 9 | 44        | 6.208755E+07 | Quito     | pichincha  | A    | 5       |
 </code></pre>
-<p>as we can see a lot of them are located in pichincha, I wonder where is
-pichincha in a map, do you know where it is? well let's find out.</p>
+<p>as we can see a lot of them are located in pichincha, I wonder where
+pichincha is on a map, do you know where it is? well let's find out.</p>
 <p>we can use geopandas to load a map of ecuador, and color the state where this
 top 10 stores are:</p>
 <pre><code>ecuador = geopandas.read_file(url)
@@ -174,7 +174,7 @@ t = pd.merge(top, provinces, left_on = 'state', right_on = 'provinces')
 </div>
 
 <p>Cool, as we can see the <em>redest</em> part of the map is where the stores sales are
-highest, since only 3 of them have stores with the most sales, we only see
+highest, since only 3 states have stores with the most sales, we only see
 those states in color:</p>
 <ul>
 <li>pichincha</li>
@@ -203,15 +203,17 @@ BEVERAGES
 GROCERY I
 </code></pre>
 <p>Now we just need to group them by month and get the mean across all the years
-at each month. Now we only need to use plolty to graph</p>
-<p style='color: red;'> <b>plotly does not want to play nice, and did not let me embed the html graph</b></p>
-<p style='color: red;'> <b>i'll leave a xkcd comic where the other graph should be</b></p>
-
-<em><a href='/public/static/plots/pyplot.html' target="_blank">click here to see the plotly graph</a></em>
+at each month. After that we use plolty to graph</p>
+<p style='color: red;'> <b>IMPORTANT!!!</b></p>
+<p style='color: red;'> <b>plotly didn't want to play nice, and did not let me embed the html chart, sorry about that.
+i'll leave a xkcd comic where the other graph should be</b></p>
+<em><a href='/public/static/plots/pyplot.html' target="_blank">but you can click here to see the plotly chart</a></em>
+<br>
 <br>
 <div class='plot'>
 <img src='https://imgs.xkcd.com/comics/convincing.png'>
 </div>
+
 
 
 <p>well, enough of that, back to the plotly graph...<p>
@@ -222,15 +224,17 @@ the bars you'll see which category each represents.</p>
 <p>As you can see even though the sales have a bump in december, the top 3
 categories do not change. It seems like they are persistent through the whole
 year.</p>
-<h2>do sales increase near holidays?</h2>
+<h2>do sales increase near national holidays?</h2>
 <p>We are all humans, and we bring to any piece of research we do our
 assumptions, our understanding of how the world works and so on. So it is only
 natural for us to think that sales would increase when a holiday (like
 Christmas) comes near. Right? </p>
-<p>Basically what we are going to do is the following:
-- get all sales from 2013
-- get all national holidays in 2013
-- see if the daily number of sales is related to the national holidays</p>
+<p>Basically what we are going to do is the following:</p>
+<ul>
+<li>get all sales from 2013</li>
+<li>get all national holidays in 2013</li>
+<li>see if the daily number of sales is related to the national holidays</li>
+<ul>
 <p>okay so using some good old fashion pure python we can get that pretty easily.
 The algorithm is O(nxm) and probably can get optimized, but I leave that for
 the reader to do :).</p>
@@ -251,17 +255,17 @@ for i, date in enumerate(s.date):
 </div>
 
 <p>As we can see in the x axis we have the days until the next holiday, and in the
-y axis the sales. The blue line is the mean of the sales at any given date.  It
+y axis the sales. The blue line is the mean of the sales at any given day.  It
 is really hard to tell if there is really a difference accordingly to how close
 the date is to a holiday. Yes, the plot shows that the day 0, 1, and 2 are the
-highest sales, but not by that many. We would have to make a full statistical
+highest sales, but not by that much. We would have to make a full statistical
 analysis to say anything concrete.</p>
 <p>But IMHO, I think we can not assume the closer we are to a holiday the more
 sales we are going to have.</p>
 <h2>final thoughts</h2>
-<p>well I hope you found the graphs pretty and insightful, if you do that's great,
-if you don't well that's is not so great.</p>
-<p>I'll leave some resources if you want to do your charts/plots/graphs:</p>
+<p>well I hope you found the graphs pretty and insightful, if you do great!
+if you didn't, well, that's is not so great.</p>
+<p>I'll leave some resources in case you want to do your charts/plots/graphs:</p>
 <ul>
 <li>
 <a href='https://mpld3.github.io'> how to make a chart/plot with mpl and d3</a>
